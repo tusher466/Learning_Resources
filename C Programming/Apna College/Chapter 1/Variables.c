42f95d2cf273dd0d327c3dbeea2f70d5bab26ecc
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-06 22:33:40 +0600

    Implement variable declarations and print statements

---
+++ C Programming/Apna College/Chapter 1/Variables.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+
+    // int number = 25;
+    // char star = '*';
+    // int age = 22;
+    // float pi = 3.1416;
+    // int _a = 30;
+
+    // printf("%d\n",number);
+    // printf("%c\n",star);
+    // printf("%d\n",age);
+    // printf("%.3f\n",pi);
+    // printf("%d\n",_a);
+
+    int age = 25;
+    float pi = 3.14;
+    char hashtag = '#';
+
+    printf("%d\n%f\n%c\n", age, pi, hashtag);
+
+    return 0;
+}
