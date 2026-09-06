e92a120226f8071026ffdb6ed939e9482558de6f
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-06 22:32:50 +0600

    Create Simple_Calculator.c for summing two integers
    This program prompts the user to enter two integers and calculates their sum.

---
+++ C Programming/Apna College/Chapter 1/Simple_Calculator.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+  
+    int a, b;
+    printf("Enter a = ");
+    scanf("%d", &a);
+
+    printf("Enter b = ");
+    scanf("%d", &b);
+
+    int sum = a + b;
+    printf("Sum is: %d\n", sum);
+
+    return 0;
+}
