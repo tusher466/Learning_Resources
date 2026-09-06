7f7d6da9bf6f79334576e263505b15c5de7b487e
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-06 22:31:12 +0600

    Add Input_Output.c for age and ID input/output

---
+++ C Programming/Apna College/Chapter 1/Input_Output.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    int age;
+    printf("Enter age:");
+    scanf("%d", &age);
+    printf("Age is: %d\n", age);
+    float Id;
+    printf("Enter Id:");
+    scanf("%f", &Id);
+    printf("Id is: %.2f\n", Id);
+
+    return 0;
+}
