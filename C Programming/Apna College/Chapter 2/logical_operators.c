02d7419acf9b6d2ffe7b9739fbb5aecded84cce6
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-07 23:23:21 +0600

    Add logical operators demonstration in C

---
+++ C Programming/Apna College/Chapter 2/logical_operators.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+// &&, ||, !
+
+int main(){
+
+    int a = 40, b = 60;
+    printf("%d\n", a && b);
+    printf("%d\n", a || b);
+    printf("%d\n", !a);
+    printf("%d\n", !b);
+
+    return 0;
+}
