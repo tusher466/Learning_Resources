40b1b33d91475a890001d8bdc7a89c09024ad1dd
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-07 23:21:25 +0600

    Create assignment_operators.c

---
+++ C Programming/Apna College/Chapter 2/assignment_operators.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+// =, +=, -=, *=, /=, %=
+
+int main(){
+
+    int a = 20, b = 10;
+    a += b;
+    printf("%d\n", a);
+
+    a -= b;
+    printf("%d\n", a);
+
+    a *= b;
+    printf("%d\n", a);
+
+    a /= b;
+    printf("%d\n", a);
+
+    a %= b;
+    printf("%d\n", a);
+
+    return 0;
+}
