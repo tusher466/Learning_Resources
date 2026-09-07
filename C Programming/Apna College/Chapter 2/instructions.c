2a72e4871ac93024f528326330ed0385fa63697a
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-07 23:22:51 +0600

    Add instructions.c with basic variable examples

---
+++ C Programming/Apna College/Chapter 2/instructions.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+    int a = 20;
+    int b = a;
+    int c = b + 1;
+    int d = 1 , e;
+
+    printf("%d\n%d\n%d\n%d\n", a, b, c, d);
+
+    int x, y, z;
+    x = y = z = 10;
+
+    printf("%d\n%d\n%d\n", x, y, z);
+
+    return 0;
+}
