a92cfca09337c52921da5f4df35b4fc672f4625f
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-08 23:19:59 +0600

    Add relational operators example in C

---
+++ C Programming/Apna College/Chapter 2/relational_operators.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+// <, <=, >, >=, ==, !=
+// True -> 1, False -> 0
+
+int main(){
+
+    int a = 20, b = 60;
+    printf("%d\n", a > b);
+    printf("%d\n", a >= b);
+    printf("%d\n", a < b);
+    printf("%d\n", a <= b);
+    printf("%d\n", a == b);
+    printf("%d\n", a != b);
+
+    return 0;
+}
