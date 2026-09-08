62257fd280f0917525e7f59248293f1eda3c02ff
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-08 23:19:11 +0600

    Create practice_7.c for MCQ check examples
    Added practice_7.c with examples of valid and invalid statements.

---
+++ C Programming/Apna College/Chapter 2/practice_7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    // mcq check
+    
+    printf("%d", 8^8); //Valid Statement
+    int x; int y = x; //Valid Statement
+    // int x, y = x; //Invalid Statement
+    // char stars = '**'; //Invalid Statement
+
+    return 0;
+    
+}
