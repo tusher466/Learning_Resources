35d8eda9c1d8b43655d223b43aa7a72b88658010
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-10 21:56:27 +0600

    Add ternary operator example in ternary.c

---
+++ C Programming/Apna College/Chapter 3/ternary.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+
+    // int age;
+    // printf("Enter your age: ");
+    // scanf("%d", &age);
+
+    // age >= 18 ? printf("Adult\n") : printf("Not adult\n");
+
+    // int marks;
+    // printf("Enter your marks: ");
+    // scanf("%d", &marks);
+
+    // marks >= 80 ? printf("You got A+\n") : printf("Try next time\n");
+
+    double pi;
+    printf("Enter the value of pi: ");
+    scanf("%lf", &pi);
+
+    pi == 3.1415 ? printf("Right\n") : printf("Wrong\n");
+
+    return 0;
+}
