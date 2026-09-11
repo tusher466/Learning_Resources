2698d5002471aaab5c930500b0fb989770662e2b
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-11 23:15:48 +0600

    Add program to print numbers from 0 to n

---
+++ C Programming/Apna College/Chapter 4/practice_14.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    int i = 0;
+    while(i <= n)
+    {
+        printf("%d ", i);
+        i++;
+    }
+    return 0;
+}
