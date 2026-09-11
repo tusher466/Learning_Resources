57a179386cefdea625b9d02c802ec16045bc9ea6
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-11 22:44:24 +0600

    Add continue statement example in C
    Implement a continue statement in a loop to skip printing the number 3.

---
+++ C Programming/Apna College/Chapter 4/continue_statement.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    for(int i = 1; i <= 5; i++)
+    {
+        if(i == 3)
+        {
+            continue;
+        }
+        printf("%d ", i);
+    }
+
+    return 0;
+}
