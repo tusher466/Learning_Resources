972d56aabd666584b42331fce66ed0d7240f9e51
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-11 22:45:02 +0600

    Add decrement_operator.c to illustrate decrement usage
    Demonstrate the usage of decrement operators in C.

---
+++ C Programming/Apna College/Chapter 4/decrement_operator.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+    //decrement operator
+    // --i(pre decrement)
+    //i--(post decrement)
+
+    int i = 5;
+    printf("%d\n", i--);
+    printf("%d\n", i);
+
+    printf("%d\n", --i);
+    printf("%d\n", i);
+
+    return 0;
+}
