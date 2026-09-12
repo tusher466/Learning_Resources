057364442c3e886ce7da496f9faf29b52a731740
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-12 09:26:04 +0600

    Add a simple Python program for personal info
    This program prints personal information and types, and takes user input for name, CGPA, and ID.

---
+++ Python/Apna College/Part 1/code.js
@@ -0,0 +1,38 @@
+# This is a simple Python program to print messages
+print ('Hello, World!')
+print ("Hello myself Md. Tusher Hossen and I am a student of Computer Science and Engineering. I am learning Python programming language. I love to code and solve problems using Python.")
+print ("I am very passionate about", 'programming', "and I want to become a professional Python developer in the future.")
+
+# Variables
+name = "Md. Tusher Hossen"
+scgpa = 3.62
+id = "253-15-466"
+Semester = 4
+_Hackathon_attended = 6
+is_student = True
+
+print("My name is:", name)
+print("My CGPA is:", scgpa)
+print("My ID is:", id)
+print("I am in Semester:", Semester)
+print("I have attended hackathon:", _Hackathon_attended)
+print("Am I a student?", is_student)
+
+print(type(name))
+print(type(scgpa))
+print(type(id))
+print(type(Semester))
+print(type(_Hackathon_attended))
+print(type(is_student))
+
+# Input from user
+user_name = input("Enter your name: ")
+user_scgpa = float(input("Enter your CGPA: "))
+user_id = input("Enter your ID: ")
+
+print("Hello, ", user_name, "!")
+print("Your CGPA is:", user_scgpa)
+print("Your ID is:", user_id)
+
+# Comments
+# This is a single-line comment
