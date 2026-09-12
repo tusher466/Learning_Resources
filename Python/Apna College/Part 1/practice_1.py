198fe27529fb86584ac410f2711cf552d7964c9a
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-12 09:27:25 +0600

    Add user input and details display in practice_1.py

---
+++ Python/Apna College/Part 1/practice_1.py
@@ -0,0 +1,26 @@
+user_first_name = input("Enter your first name: ")
+user_last_name = input("Enter your last name: ")
+user_age = int(input("Enter your age: "))
+user_height = float(input("Enter your height in meters: "))
+
+print("Hello, ", user_first_name, user_last_name, "!")
+print("Your age is:", user_age)
+print("Your height is:", user_height, "meters")
+
+# Store Tony's details in variables
+first_name = "Tony"
+last_name = "Stark"
+age = 53
+height = 1.85
+is_superhero = True
+# Ask the user for Tony's superhero name
+superhero_name = input("Enter Tony's superhero name: ")
+# Print all the details
+print("--- Person Details ---")
+print("First name:", first_name)
+print("Last name:", last_name)
+5
+print("Age:", age)
+print("Height:", height, "m")
+print("Is a superhero:", is_superhero)
+print("Superhero name:", superhero_name)
