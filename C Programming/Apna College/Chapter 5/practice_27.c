9fa0eaca149c80360c031d3480d7524aac826b5c
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-14 23:33:48 +0600

    Add functions to print greetings and farewells

---
+++ C Programming/Apna College/Chapter 5/practice_27.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void printHello();
+void printGoodbye();
+
+int main(){
+
+    printHello();
+    printGoodbye();
+
+    return 0;
+
+}
+
+void printHello(){
+    printf("Hello\n");
+}
+
+void printGoodbye(){
+    printf("good bye\n");
+}
