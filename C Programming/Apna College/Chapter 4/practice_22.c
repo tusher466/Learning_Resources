537e5c1fc301c21026b8859ae133b5b2c52f0391
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-14 23:21:31 +0600

    Create practice_22.c

---
+++ C Programming/Apna College/Chapter 4/practice_22.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(int i = 10; i >= 1; i--)
+    {
+        printf("%d x %d = %d\n", n, i, (n * i));
+    }
+
+    return 0;
+}
