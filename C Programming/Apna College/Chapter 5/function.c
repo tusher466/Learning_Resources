d0f0bc0922158486fdc4af779bd094be17fc34eb
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-14 23:32:22 +0600

    Add function to print greetings in function.c

---
+++ C Programming/Apna College/Chapter 5/function.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+//Function Prototype
+void printHello();
+
+//Function Call
+int main(){
+
+    printHello();
+    printHello();
+    return 0;
+}
+
+//Function Definition
+void printHello()
+{
+    printf("Hello World\n");
+    printf("My name is Efti\n");
+}
