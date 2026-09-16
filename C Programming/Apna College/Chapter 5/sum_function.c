150cca551e6ea49433b6ac6e7cfeae27fc80a5ce
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-16 23:53:32 +0600

    Create sum_function.c

---
+++ C Programming/Apna College/Chapter 5/sum_function.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int sum(int a, int b);
+
+int main(){
+
+    int a, b;
+    printf("Enter two value: ");
+    scanf("%d%d", &a, &b);
+    
+    int s = sum(a, b);
+    printf("Sum is: %d", s);
+    return 0;
+
+}
+
+int sum(int a,int b){
+
+    return a + b;
+}
