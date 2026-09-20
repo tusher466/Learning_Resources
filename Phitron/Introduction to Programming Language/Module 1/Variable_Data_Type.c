09852c200bbe849f06dd6ce8acea252e4d9bff89
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-20 19:08:07 +0600

    Add Variable_Data_Type.c with basic variable types

---
+++ Phitron/Introduction to Programming Language/Module 1/Variable_Data_Type.c
@@ -0,0 +1,15 @@
+//
+// Created by eftia on 10/26/2025.
+//
+// Phitron Module: 1
+
+#include <stdio.h>
+int main() {
+
+    int a = 10;
+    float b = 10.5;
+    char c = 'a';
+    printf("%d\n%f\n%c\n", a, b, c);
+
+    return 0;
+}
