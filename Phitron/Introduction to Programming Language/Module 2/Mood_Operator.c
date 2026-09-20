74f746f82e85741b5203518a8cd816edbb363a74
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-20 19:11:14 +0600

    Add Mood_Operator.c to calculate remainder

---
+++ Phitron/Introduction to Programming Language/Module 2/Mood_Operator.c
@@ -0,0 +1,15 @@
+//
+// Created by eftia on 10/27/2025.
+//
+// Phitron Module: 2
+
+#include <stdio.h>
+int main() {
+
+    int a = 12;
+    int b = 5;
+    int rem = a % b;
+    printf("The remainder is %d\n", rem);
+
+    return 0;
+}
