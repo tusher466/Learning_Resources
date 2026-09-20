1f8d748735fc0d44fb0069fd171e5e8d6669ee2e
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-20 19:06:11 +0600

    Add Bool.c to demonstrate boolean values

---
+++ Phitron/Introduction to Programming Language/Module 1/Bool.c
@@ -0,0 +1,17 @@
+//
+// Created by eftia on 10/26/2025.
+//
+// Phitron Module: 1
+
+#include <stdio.h>
+#include <stdbool.h>
+int main() {
+
+    bool a, b;
+    a = true;
+    b = false;
+    printf("%d\n", a);
+    printf("%d\n", b);
+
+    return 0;
+}
