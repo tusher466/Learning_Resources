b277e7b03888d61557f8308b4968249176a8d373
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-20 19:10:42 +0600

    Add if-else ladder program in C

---
+++ Phitron/Introduction to Programming Language/Module 2/if_else_ladder.c
@@ -0,0 +1,24 @@
+//
+// Created by eftia on 10/27/2025.
+//
+// Phitron Module: 2
+
+#include <stdio.h>
+int main() {
+
+    int tk;
+    scanf("%d", &tk);
+    if (tk >= 100)
+    {
+        printf("Burger Khabo\n");
+    }
+    else if (tk >= 50)
+    {
+        printf("Fusca Khabo\n");
+    }
+    else {
+        printf("Kichue Khabo Na\n");
+    }
+
+    return 0;
+}
