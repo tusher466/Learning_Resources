b39829705b4de38da9c7fd8bf6e5af3183647396
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-20 19:11:49 +0600

    Add nested if-else structure in Nested_if_else.c
    Implement nested if-else logic for travel decisions based on budget.

---
+++ Phitron/Introduction to Programming Language/Module 2/Nested_if_else.c
@@ -0,0 +1,26 @@
+//
+// Created by eftia on 10/27/2025.
+//
+// Phitron Module: 2
+
+#include <stdio.h>
+int main() {
+
+    int tk;
+    scanf("%d", &tk);
+    if (tk >= 5000) {
+        printf("Cox's Bazar Jabo\n");
+        if (tk >= 10000) {
+            printf("Saint Martin Jabo\n");
+
+        }
+        else {
+            printf("Back Korbo\n");
+        }
+    }
+    else {
+        printf("Kotaoo Jabo Na\n");
+    }
+
+    return 0;
+}
