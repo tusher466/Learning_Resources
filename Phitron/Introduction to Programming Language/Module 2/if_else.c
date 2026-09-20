32e3689bbb43e4549757359dae87eadca236b364
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-20 19:09:54 +0600

    Add if-else example for user input handling

---
+++ Phitron/Introduction to Programming Language/Module 2/if_else.c
@@ -0,0 +1,21 @@
+//
+// Created by eftia on 10/27/2025.
+//
+// Phitron Module: 2
+
+#include <stdio.h>
+int main() {
+
+    int tk;
+    scanf("%d", &tk);
+    if (tk >= 100)
+    {
+        printf("Burger Khabo\n");
+    }
+    else
+    {
+        printf("Burger Khabo Na\n");
+    }
+
+    return 0;
+}
