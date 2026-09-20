668348a421525ec5ef85ab05056b0e01af135102
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-20 19:07:29 +0600

    Add Input.c for basic input and output operations

---
+++ Phitron/Introduction to Programming Language/Module 1/Input.c
@@ -0,0 +1,19 @@
+//
+// Created by eftia on 10/26/2025.
+//
+// Phitron Module: 1
+
+#include <stdio.h>
+int main() {
+
+    int a;
+    char b;
+    float c;
+    scanf("%d", &a);
+    scanf(" %c", &b);
+    scanf("%f", &c);
+
+    printf("%d\n%c\n%f\n", a, b, c);
+
+    return 0;
+}
