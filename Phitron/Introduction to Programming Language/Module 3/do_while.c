2cbf2176101cecdf7341d473f52b74dd2283b2fa
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-21 22:57:44 +0600

    Add do-while loop example in do_while.c

---
+++ Phitron/Introduction to Programming Language/Module 3/do_while.c
@@ -0,0 +1,18 @@
+//
+// Created by eftia on 12/11/2025.
+//
+// Phitron Module: 3
+
+#include <stdio.h>
+int main() {
+
+    int n;
+    scanf("%d", &n);
+    do {
+        printf("%d\n", n);
+        n--;
+    }
+    while (n > 0);
+
+    return 0;
+}
