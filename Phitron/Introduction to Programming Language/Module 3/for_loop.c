43b9d764f366659254e46c926875a270a116b3bc
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-21 22:58:08 +0600

    Add for_loop.c to print 'Hello World' 30 times

---
+++ Phitron/Introduction to Programming Language/Module 3/for_loop.c
@@ -0,0 +1,14 @@
+//
+// Created by eftia on 10/29/2025.
+//
+// Phitron Module: 3
+
+#include <stdio.h>
+int main() {
+
+    for(int i = 1; i <= 30; i++) {
+        printf(" %d Hello World\n", i);
+    }
+
+    return 0;
+}
