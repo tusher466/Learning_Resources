cc4add0b7467d2d2b026018004513ff1f3226a17
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-21 22:56:49 +0600

    Add break statement in loop in break.c
    Implement a loop that breaks when i equals 5.

---
+++ Phitron/Introduction to Programming Language/Module 3/break.c
@@ -0,0 +1,17 @@
+//
+// Created by eftia on 10/30/2025.
+//
+// Phitron Module: 3
+
+#include <stdio.h>
+int main() {
+
+    for (int i = 1; i <= 10; i++) {
+        if (i == 5) {
+            break;
+        }
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
