287ab71f4331065e3574eb5c580b39f44275f26e
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-21 22:59:59 +0600

    Create while.c

---
+++ Phitron/Introduction to Programming Language/Module 3/while.c
@@ -0,0 +1,17 @@
+//
+// Created by eftia on 12/11/2025.
+//
+// Phitron Module: 3
+
+#include <stdio.h>
+int main() {
+
+    int n;
+    scanf("%d", &n);
+    while (n > 0) {
+        printf("%d\n", n);
+        n--;
+    }
+
+    return 0;
+}
