fce34e824bfedc48ac4d706fff34b2e0c19a5d99
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-21 22:59:13 +0600

    Add more_for_loop.c to demonstrate for loops

---
+++ Phitron/Introduction to Programming Language/Module 3/more_for_loop.c
@@ -0,0 +1,18 @@
+//
+// Created by eftia on 10/30/2025.
+//
+// Phitron Module: 3
+
+#include <stdio.h>
+int main() {
+
+    for (int i = 100; i >= 0; i--) {
+        printf("%d\n", i);
+    }
+
+    for (int i = 0; i <= 100; i++) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
