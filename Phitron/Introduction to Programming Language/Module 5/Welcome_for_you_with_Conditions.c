d3c39ad0efccecf1877f296f6b57d532dd4e534b
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-22 23:14:37 +0600

    Create Welcome_for_you_with_Conditions.c

---
+++ Phitron/Introduction to Programming Language/Module 5/Welcome_for_you_with_Conditions.c
@@ -0,0 +1,20 @@
+//
+// Created by eftia on 12/11/2025.
+//
+// Problem: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/I
+
+#include <stdio.h>
+int main ()
+{
+    int A, B;
+    scanf("%d %d", &A, &B);
+    if (A >= B) {
+        printf("Yes\n");
+    }
+    else {
+        printf("No\n");
+    }
+
+
+    return 0;
+}
