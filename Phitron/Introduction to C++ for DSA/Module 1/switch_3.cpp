64e81a20772674adddc4526ef303dfeb8d1465b6
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-23 20:51:21 +0600

    Add switch case to identify vowels and consonants

---
+++ Phitron/Introduction to C++ for DSA/Module 1/switch_3.cpp
@@ -0,0 +1,23 @@
+//
+// Created by eftia on 11/18/2025.
+//
+//Phitron Module: 1
+
+#include <iostream>
+using namespace std;
+int main() {
+
+    char c;
+    cin >> c;
+    switch (c)
+    {
+        case 'A': cout << "Vowel"; break;
+        case 'E': cout << "Vowel"; break;
+        case 'I': cout << "Vowel"; break;
+        case 'O': cout << "Vowel"; break;
+        case 'U': cout << "Vowel"; break;
+        default: cout << "Consonant";
+    }
+
+    return 0;
+}
