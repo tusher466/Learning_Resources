0eeebf04a4929f171ce638c6e67f22e7dbf1684d
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-23 21:13:13 +0600

    Implement dynamic array size increase example
    This program demonstrates dynamic array size increase in C++. It initializes an array, copies its elements to a new larger array, and adds additional elements.

---
+++ Phitron/Introduction to C++ for DSA/Module 2/dynamic_array_size_incrase.cpp
@@ -0,0 +1,27 @@
+//
+// Created by eftia on 11/19/2025.
+//
+//Phitron Module: 2
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int *a = new int[3];
+    for (int i = 0; i < 3; i++) {
+        cin >> a[i];
+    }
+
+    int *b = new int[5];
+    for (int i = 0; i < 3; i++) {
+        b[i] = a[i];
+    }
+    b[3] = 40;
+    b[4] = 50;
+    delete [] a;
+    for (int i = 0; i < 5; i++) {
+        cout << b[i] << " ";
+    }
+
+    return 0;
+}
