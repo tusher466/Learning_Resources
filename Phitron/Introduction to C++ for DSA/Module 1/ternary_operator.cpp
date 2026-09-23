1280316f0e2d70680910a900c7a769a572c9e2b2
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-23 20:51:47 +0600

    Add ternary operator example in C++

---
+++ Phitron/Introduction to C++ for DSA/Module 1/ternary_operator.cpp
@@ -0,0 +1,15 @@
+//
+// Created by eftia on 11/18/2025.
+//
+//Phitron Module: 1
+
+#include <iostream>
+using namespace std;
+int main() {
+
+    int x;
+    cin >> x;
+    cout << (x % 2 ==0 ? "x is even" : "x is odd") << endl;
+
+    return 0;
+}
