cb4be923bfe8434c7456b33c55a9f6ffe33027c5
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-23 20:47:48 +0600

    Add if_else.cpp to check even or odd numbers

---
+++ Phitron/Introduction to C++ for DSA/Module 1/if_else.cpp
@@ -0,0 +1,19 @@
+//
+// Created by eftia on 11/18/2025.
+//
+//Phitron Module: 1
+
+#include <iostream>
+using namespace std;
+int main() {
+
+    int x;
+    cin >> x;
+    if (x % 2 == 0) {
+        cout << "x is Even" << endl;
+    } else {
+        cout << "x is Odd" << endl;
+    }
+
+    return 0;
+}
