9053b7967e612e5ba47518ab837a949baebe7485
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-23 21:13:33 +0600

    Add dynamic_variable.cpp for dynamic memory usage

---
+++ Phitron/Introduction to C++ for DSA/Module 2/dynamic_variable.cpp
@@ -0,0 +1,16 @@
+//
+// Created by eftia on 11/19/2025.
+//
+//Phitron Module: 2
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int x = 10;
+    int *p = new int;
+    *p = 500;
+    cout << *p << endl;
+
+    return 0;
+}
