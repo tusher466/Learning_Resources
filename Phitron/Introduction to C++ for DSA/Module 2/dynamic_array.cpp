9b1e70f87b7b71456280e27fae25095527b0cf81
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-23 21:12:39 +0600

    Create dynamic_array.cpp

---
+++ Phitron/Introduction to C++ for DSA/Module 2/dynamic_array.cpp
@@ -0,0 +1,19 @@
+//
+// Created by eftia on 11/19/2025.
+//
+//Phitron Module: 2
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int *a = new int[5];
+    for (int i = 0; i < 5; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < 5; i++) {
+        cout << a[i] << endl;
+    }
+
+    return 0;
+}
