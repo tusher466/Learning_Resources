957b449f7aebb43be65c3767d8ae1425488527d5
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-23 20:49:33 +0600

    Add setprecision example in C++
    This program demonstrates the use of setprecision to format output.

---
+++ Phitron/Introduction to C++ for DSA/Module 1/setprecision.cpp
@@ -0,0 +1,16 @@
+//
+// Created by eftia on 11/18/2025.
+//
+//Phitron Module: 1
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+
+    double x = 34.697452;
+    cout << x << endl;
+    cout << fixed << setprecision(2) << x << endl;
+
+    return 0;
+}
