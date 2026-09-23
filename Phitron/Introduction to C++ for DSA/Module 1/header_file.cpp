880fb3fc90b327253e16837b18204dd2678d5c0b
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-23 20:47:22 +0600

    Add header_file.cpp for C++ module 1
    Created a header file for C++ module with necessary includes.

---
+++ Phitron/Introduction to C++ for DSA/Module 1/header_file.cpp
@@ -0,0 +1,12 @@
+//
+// Created by eftia on 11/19/2025.
+//
+//Photron Module: 1
+
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+
+// Ato gula add na kore just akta header file declare korle C++ r sob kaj kora jabe
+
+#include <bits/stdc++.h>
