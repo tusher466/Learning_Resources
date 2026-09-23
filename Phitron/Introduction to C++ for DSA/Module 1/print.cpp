71b05c533fd379da8d41f4ee186f42d6980d646b
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-23 20:49:08 +0600

    Create print.cpp

---
+++ Phitron/Introduction to C++ for DSA/Module 1/print.cpp
@@ -0,0 +1,17 @@
+//
+// Created by eftia on 11/17/2025.
+//
+//Phitron Module: 1
+
+#include <iostream>
+int main() {
+
+    std::cout << "Hello World!\n";
+    int x = 100;
+    char c = 'E';
+    double d = 3.14;
+    std::cout << x << '\n' << c << std::endl << d << '\n';
+    std::cout << "I love " << x << std::endl;
+
+    return 0;
+}
