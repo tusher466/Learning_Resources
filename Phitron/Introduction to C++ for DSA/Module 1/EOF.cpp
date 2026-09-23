f9265dd42b5edd0d94d44990f2d5d9edb4e7636d
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-23 20:46:45 +0600

    Add EOF handling example in C++

---
+++ Phitron/Introduction to C++ for DSA/Module 1/EOF.cpp
@@ -0,0 +1,16 @@
+//
+// Created by eftia on 11/18/2025.
+//
+//Phitron Module: 1
+
+#include <iostream>
+using namespace std;
+int main() {
+
+    int x;
+    while (cin >> x) {
+        cout << x << endl;
+    }
+
+    return 0;
+}
