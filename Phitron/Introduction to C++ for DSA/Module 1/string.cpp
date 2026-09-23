d17c25469c89b70b4733b6236f2b0c7624b89591
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-23 20:49:59 +0600

    Add string input/output example in string.cpp

---
+++ Phitron/Introduction to C++ for DSA/Module 1/string.cpp
@@ -0,0 +1,15 @@
+//
+// Created by eftia on 11/18/2025.
+//
+//Phitron Module: 1
+
+#include <iostream>
+using namespace std;
+int main() {
+
+    char s[100];
+    cin.getline(s,100);
+    cout << s << endl;
+
+    return 0;
+}
