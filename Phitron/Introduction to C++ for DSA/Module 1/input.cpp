00d9ad18b85ae61e657532573be2e6aeac7fa095
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-23 20:48:12 +0600

    Create input.cpp

---
+++ Phitron/Introduction to C++ for DSA/Module 1/input.cpp
@@ -0,0 +1,19 @@
+//
+// Created by eftia on 11/17/2025.
+//
+//Phitron Module: 1
+
+#include <iostream>
+using namespace std;
+int main() {
+
+    int x;
+    char c;
+    cin  >> x;
+    cin  >> c;
+    cout << x <<  endl;
+    cout << c << endl;
+    cout << (int) c << endl;
+
+    return 0;
+}
