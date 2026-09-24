9b367e1fc6ee25c68581d93e1dbc9d1776867cce
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-24 23:27:00 +0600

    Add Student class with constructor and demo
    This file defines a Student class with a constructor and demonstrates its usage in the main function.

---
+++ Phitron/Introduction to C++ for DSA/Module 3/constructor.cpp
@@ -0,0 +1,34 @@
+//
+// Created by eftia on 11/20/2025.
+//
+//Phitron Module: 3
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student{
+
+    public:
+    int roll;
+    int cls;
+    double gpa;
+
+    Student(int r, int c, double g)
+    {
+        roll = r;
+        cls = c;
+        gpa = g;
+    }
+
+};
+
+int main(){
+
+    Student rahim(45,5,4.50);
+    Student karim(2,5,5.00);
+
+    cout << rahim.roll << " " << rahim.cls << " " << rahim.gpa << endl;
+    cout << karim.roll << " " << karim.cls << " " << karim.gpa << endl;
+
+    return 0;
+}
