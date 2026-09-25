ef57ff7045dd146376e71e414a567938242d7ecc
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-25 23:37:45 +0600

    Add string manipulation example in string.cpp
    Initial implementation of a C++ program demonstrating string manipulation.

---
+++ Phitron/Introduction to C++ for DSA/Module 5/string.cpp
@@ -0,0 +1,27 @@
+//
+// Created by eftia on 11/28/2025.
+//
+//Phitron Module: 5
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+     // char s[10] = "Hello";
+     // strcpy(s, "World");
+     // string s = "Hello";
+     // s = "World";
+     // s += " World";
+     string s = "Hello World";
+     s = "Hi! How are you? I'm fine";
+     // string s2 = "Hello World";
+     // if (s == s2) {
+     //      cout << "Strings are equal" << endl;
+     // }
+     // else {
+     //      cout << "Strings are not equal" << endl;
+     // }
+     cout << s << endl;
+
+     return 0;
+}
