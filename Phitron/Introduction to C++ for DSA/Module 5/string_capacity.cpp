d78ace912b5cc19eca85b4677970922f4ca0d8f9
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-25 23:38:13 +0600

    Add string capacity demonstration in string_capacity.cpp
    This program demonstrates string resizing and output in C++. It initializes a string, resizes it, and prints the result.

---
+++ Phitron/Introduction to C++ for DSA/Module 5/string_capacity.cpp
@@ -0,0 +1,25 @@
+//
+// Created by eftia on 11/28/2025.
+//
+//Phitron Module: 5
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    string s = "Hello World";
+    s.resize(15,'n');
+    // cout << s.size() << endl;
+    // cout << s.max_size() << endl;
+    // cout << s.capacity() << endl;
+    // s.clear();
+    cout << s << endl;
+    // if (s.empty()==true) {
+    //     cout << "String is empty" << endl;
+    // }
+    // else {
+    //     cout << "String is not empty" << endl;
+    // }
+
+    return 0;
+}
