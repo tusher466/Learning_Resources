d0ff3d88576a129e4074b8b2ced14b31e9bde8dc
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-25 23:39:05 +0600

    Create string_modifiers.cpp

---
+++ Phitron/Introduction to C++ for DSA/Module 5/string_modifiers.cpp
@@ -0,0 +1,25 @@
+//
+// Created by eftia on 11/28/2025.
+//
+//Phitron Module: 5
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    string s = "Hello World";
+    // s.push_back('!');
+    // string s2 = "Hi";
+    // s += s2;
+    // s.append(s2);
+    // s.pop_back();
+    // s = s2;
+    // s.assign(s2);
+    // s.erase(0,5);
+    // s.replace(0,5,"Goodbye");
+    // s.insert(0,"Hello ");
+    cout << s << endl;
+
+
+    return 0;
+}
