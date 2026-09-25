bccecec040a029ec3e86147021efa39fd6159f3c
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-25 23:35:54 +0600

    Add element access demonstration for strings in C++
    This program demonstrates various methods to access elements of a string in C++. It reads a string and outputs the second character, the first character, and the last character.

---
+++ Phitron/Introduction to C++ for DSA/Module 5/element_access.cpp
@@ -0,0 +1,19 @@
+//
+// Created by eftia on 11/28/2025.
+//
+//Phitron Module: 5
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    string s;
+    cin >> s;
+    cout << s[1] << endl;
+    // cout << s.at(0) << endl;
+    cout << s.front() << endl;
+    cout << s.back() << endl;
+    // cout << s[s.size()-1] << endl; // s[ n - 1]
+
+    return 0;
+}
