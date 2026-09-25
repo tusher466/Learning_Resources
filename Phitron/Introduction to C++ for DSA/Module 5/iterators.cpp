42de17e16203b63f4e22333aff9c0b377a4e5fdd
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-25 23:36:24 +0600

    Add iterator example for string character output
    Implement iterator to print each character of a string.

---
+++ Phitron/Introduction to C++ for DSA/Module 5/iterators.cpp
@@ -0,0 +1,21 @@
+//
+// Created by eftia on 11/29/2025.
+//
+//Phitron Module: 5
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    string s = "Hello World";
+    // for (int i = 0; i < s.size(); i++) {
+    //     cout << s[i] << endl;
+    // }
+    // cout << *s.begin() << endl;
+    // cout << *(s.end() - 1) << endl;
+    for (auto it = s.begin(); it != s.end(); it++) {
+        cout << *it << endl;
+    }
+
+    return 0;
+}
