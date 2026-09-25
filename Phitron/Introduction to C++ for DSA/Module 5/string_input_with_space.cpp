b0393ab57a70b658cc1cd17962eb115ed0032e39
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-25 23:38:36 +0600

    Create string_input_with_space.cpp

---
+++ Phitron/Introduction to C++ for DSA/Module 5/string_input_with_space.cpp
@@ -0,0 +1,19 @@
+//
+// Created by eftia on 11/29/2025.
+//
+//Phitron Module: 5
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int x;
+    cin >> x;
+    string s;
+    // cin.getline(s, 100);
+    getline(cin, s);
+    cout << x << endl;
+    cout << s << endl;
+
+    return 0;
+}
