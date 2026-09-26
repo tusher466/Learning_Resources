fbec1c374cc657192bc9c5ee73475f8e77ed7696
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-26 22:32:13 +0600

    Add examples of string constructors in string_constructor.cpp
    Implement string constructor examples in C++.

---
+++ Phitron/Introduction to C++ for DSA/Module 6/string_constructor.cpp
@@ -0,0 +1,22 @@
+//
+// Created by eftia on 11/30/2025.
+//
+//Phitron Module: 6
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    // string s = "Hello";
+    //1
+    // string s("Hello");
+    //2
+    // string s("Hello World", 7);
+    //3
+    // string s("Hello");
+    // string t(s,4);
+    //4
+    string s(5,'A');
+    cout << s << endl;
+    return 0;
+}
