65f75b32fb3344554c4c7912bd0e47b9cbdbd766
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-26 22:30:46 +0600

    Create range_based_for_loop.cpp

---
+++ Phitron/Introduction to C++ for DSA/Module 6/range_based_for_loop.cpp
@@ -0,0 +1,21 @@
+//
+// Created by eftia on 11/30/2025.
+//
+//Phitron Module: 6
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    string s;
+    cin >> s;
+    // for (int i = 0; i < s.size(); i++) {
+    //     cout << s[i] << endl;
+    // }
+    for (char c : s) {
+        cout << c << endl; // (char c : s
+    }
+
+
+    return 0;
+}
